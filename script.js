d21dbcae5eea354c2822f5d40d789c28bfdb7f62
richardd1634 <richardd1634@gmail.com>
2022-02-14 20:36:59 -0500

    Add final result
    Add alert message when a player reaches 5 points and also reset the game.

--- script.js
+++ script.js
@@ -10,47 +10,69 @@ function game() {
   const btns = document.querySelectorAll("button");
 
   btns.forEach((btn) => {
-    btn.addEventListener("click", (event) =>
-      playRound(event.target.textContent)
-    );
+    btn.addEventListener("click", (event) => playRound(event.target.id));
   });
 
   function playRound(playerSelection) {
     const computerSelection = getComputerSelection();
 
-    if (playerSelection === computerSelection) {
-      draws++;
-      result.textContent = "Draw!";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
+    if (playerSelection === "btn-rock" && computerSelection === "paper") {
       computerScore++;
       result.textContent = "You lose! paper beats rock";
-    } else if (playerSelection === "rock" && computerSelection === "scissor") {
+    } else if (
+      playerSelection === "btn-rock" &&
+      computerSelection === "scissor"
+    ) {
       playerScore++;
       result.textContent = "You win! rock beats scissor";
-    } else if (playerSelection === "paper" && computerSelection === "scissor") {
+    } else if (
+      playerSelection === "btn-paper" &&
+      computerSelection === "scissor"
+    ) {
       computerScore++;
       result.textContent = "You lose! scissor beats paper";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
+    } else if (
+      playerSelection === "btn-paper" &&
+      computerSelection === "rock"
+    ) {
       playerScore++;
       result.textContent = "You win! paper beats rock";
-    } else if (playerSelection === "scissor" && computerSelection === "rock") {
+    } else if (
+      playerSelection === "btn-scissor" &&
+      computerSelection === "rock"
+    ) {
       computerScore++;
       result.textContent = "You lose! rock beats scissor";
-    } else if (playerSelection === "scissor" && computerSelection === "paper") {
+    } else if (
+      playerSelection === "btn-scissor" &&
+      computerSelection === "paper"
+    ) {
       playerScore++;
       result.textContent = "You win! scissor beats paper";
+    } else {
+      draws++;
+      result.textContent = "Draw!";
     }
     score.textContent = printScore();
+    checkWinner();
   }
 
-  function printResult() {
-    if (playerScore >= 5) {
-      return "You are the winner!";
-    } else if (playerScore < computerScore) {
-      return "You are the loser!";
-    } else {
-      return "You win! " + printScore();
+  function checkWinner() {
+    if (playerScore === 5) {
+      alert("Congratulations! You win this round");
+      resetGame();
+    } else if (computerScore === 5) {
+      alert("Sorry, you lose this round");
+      resetGame();
     }
+  }
+
+  function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    draws = 0;
+    result.textContent = "";
+    score.textContent = "";
   }
 
   function printScore() {
